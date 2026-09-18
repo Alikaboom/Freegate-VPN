f74d3e85158534365e31c1234a462a8054d9cb00
Alikaboom <alireza.azizi0522@gmail.com>
2026-09-18 08:31:12 +0300

    feat: expand user model to track name and purchased plans

--- models/User.js
+++ models/User.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    createdAt: { type: Date, default: Date.now }
+    name: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    plans: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Product' 
+    }],
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
